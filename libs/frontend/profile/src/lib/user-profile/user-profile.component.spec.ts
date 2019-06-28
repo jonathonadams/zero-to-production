77398415382c18ffc9769dedf98d6e2dfe5b4362
Jonathon Adams <jono.adams22@gmail.com>
2019-06-28 13:24:49 +0930

    Fixed failing tests.

--- libs/frontend/profile/src/lib/user-profile/user-profile.component.spec.ts
+++ libs/frontend/profile/src/lib/user-profile/user-profile.component.spec.ts
@@ -3,7 +3,6 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { UserProfileComponent } from './user-profile.component';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { AuthUserFacade } from '@workspace/frontend/data-access/user-auth';
 import { UsersFacade } from '@workspace/frontend/data-access/users';
 import { ThemeService } from '@workspace/frontend/common/theme';
 import { of } from 'rxjs';
@@ -11,14 +10,12 @@ import { of } from 'rxjs';
 describe('UserProfileComponent', () => {
   let component: UserProfileComponent;
   let fixture: ComponentFixture<UserProfileComponent>;
-  let authFacade: AuthUserFacade;
   let userFacade: UsersFacade;
   let themeService: ThemeService;
 
-  const authFacadeSpy = {
+  const userFacadeSpy = {
     authUser$: of(jest.fn())
   };
-  const userFacadeSpy = {};
   const themServiceSpy = {};
 
   beforeEach(async(() => {
@@ -26,14 +23,12 @@ describe('UserProfileComponent', () => {
       imports: [ReactiveFormsModule],
       declarations: [UserProfileComponent],
       providers: [
-        { provide: AuthUserFacade, useValue: authFacadeSpy },
         { provide: UsersFacade, useValue: userFacadeSpy },
         { provide: ThemeService, useValue: themServiceSpy }
       ],
       schemas: [NO_ERRORS_SCHEMA]
     }).compileComponents();
 
-    authFacade = TestBed.get<AuthUserFacade>(AuthUserFacade);
     userFacade = TestBed.get<UsersFacade>(UsersFacade);
     themeService = TestBed.get<ThemeService>(ThemeService);
   }));
