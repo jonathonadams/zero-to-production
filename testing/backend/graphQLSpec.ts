0ab8bd71e65c4cfb4d01b5c579b5befe2b250c8b
Jonathon Adams <jono.adams22@gmail.com>
2019-07-07 17:02:59 +0930

    Added the test helpers.

---
+++ testing/backend/graphQLSpec.ts
@@ -0,0 +1,184 @@
+import 'jest-extended';
+import mongoose from 'mongoose';
+import { runQuery, setupTestDB } from './helpers';
+import { signAccessToken } from '../server/auth/auth';
+import { ExecutionResultDataDefault } from 'graphql/execution/execute';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import { User } from '@workspace/shared/data';
+import { GraphQLSchema } from 'graphql';
+
+/**
+ * Object.keys(object) is used to return an array of the names of object properties.
+ * This can be used to create abstracted values to create the query strings
+ * Example of a query string
+ *
+ * @param model
+ * @param resourceName
+ * @param resourceToCreate
+ * @param resourceToUpdate
+ * @param testDependents
+ */
+export default function createGraphQLSpec<T>(schema: GraphQLSchema) {
+  return function(
+    model: any,
+    resourceName: string,
+    resourceToCreate: any,
+    resourceToUpdate: any
+  ) {
+    if (!resourceToCreate || Object.keys(resourceToCreate).length === 0) {
+      throw new Error(
+        'Must provide an object to create with properties of at least length 1'
+      );
+    }
+
+    if (!resourceToUpdate || Object.keys(resourceToUpdate).length === 0) {
+      throw new Error(
+        'Must provide an object to updated with properties of at least length 1'
+      );
+    }
+
+    // GraphQL schemas are designed written with UpperCase names
+    const upperResourceName =
+      resourceName.charAt(0).toUpperCase() + resourceName.slice(1);
+
+    describe(`GraphQL / ${upperResourceName}`, () => {
+      let mongoServer: MongoMemoryServer;
+      let db: mongoose.Mongoose;
+      let resource: T;
+      let jwt: string;
+
+      beforeAll(async () => {
+        ({ db, mongoServer } = await setupTestDB());
+        jwt = signAccessToken({ id: '1', role: 0 } as User);
+
+        resource = await model.create(resourceToCreate);
+      });
+
+      afterAll(async () => {
+        await db.disconnect();
+        await mongoServer.stop();
+      });
+
+      describe(`new${upperResourceName}($input: New${upperResourceName}Input!)`, () => {
+        it(`should create a new ${upperResourceName}`, async () => {
+          const queryName = `new${upperResourceName}`;
+          const result = await runQuery(schema)(
+            `
+            mutation New${upperResourceName}($input: New${upperResourceName}Input!) {
+              ${queryName}(input: $input) {
+                id
+              }
+            }
+          `,
+            { input: resourceToCreate },
+            jwt
+          );
+
+          expect(result.errors).not.toBeDefined();
+          expect(
+            (result.data as ExecutionResultDataDefault)[queryName]
+          ).toBeObject();
+          expect(
+            (result.data as ExecutionResultDataDefault)[queryName].id
+          ).toBeString();
+        });
+      });
+
+      describe(`all${upperResourceName}s`, () => {
+        it(`should return all ${resourceName}s`, async () => {
+          const queryName = `all${upperResourceName}s`;
+          const result = await runQuery(schema)(
+            `
+          {
+            ${queryName} {
+              id
+              ${Object.keys(resourceToCreate)[0]}
+            }
+          }`,
+            {},
+            jwt
+          );
+
+          expect(result.errors).not.toBeDefined();
+          expect(
+            (result.data as ExecutionResultDataDefault)[queryName]
+          ).toBeArray();
+        });
+      });
+
+      describe(`${resourceName}(id: ID!)`, () => {
+        it(`should return a ${resourceName} by id`, async () => {
+          const queryName = `${resourceName}`;
+
+          const result = await runQuery(schema)(
+            `
+        {
+          ${queryName}(id: "${(resource as any).id}") {
+            id
+          }
+        }`,
+            {},
+            jwt
+          );
+
+          expect(result.errors).not.toBeDefined();
+          expect(
+            (result.data as ExecutionResultDataDefault)[queryName]
+          ).toBeObject();
+          expect(
+            (result.data as ExecutionResultDataDefault)[queryName].id
+          ).toEqual((resource as any).id.toString());
+        });
+      });
+
+      describe(`update${upperResourceName}($input: Updated${upperResourceName}Input!)`, () => {
+        it(`should update an ${upperResourceName}`, async () => {
+          const queryName = `update${upperResourceName}`;
+
+          resourceToUpdate.id = (resource as any).id;
+
+          const result = await runQuery(schema)(
+            `
+            mutation Update${upperResourceName}($input: Updated${upperResourceName}Input!) {
+              ${queryName}(input: $input) {
+                id
+              }
+            }
+          `,
+            { input: resourceToUpdate },
+            jwt
+          );
+
+          expect(result.errors).not.toBeDefined();
+          expect(
+            (result.data as ExecutionResultDataDefault)[queryName]
+          ).toBeObject();
+          expect(
+            (result.data as ExecutionResultDataDefault)[queryName].id
+          ).toEqual((resource as any).id.toString());
+        });
+      });
+
+      describe(`remove${upperResourceName}($id: ID!)`, () => {
+        it(`should delete a ${upperResourceName} by id`, async () => {
+          const queryName = `remove${upperResourceName}`;
+          const result = await runQuery(schema)(
+            `
+            mutation Remove${upperResourceName}($id: ID!) {
+              ${queryName}(id: $id) {
+                id
+              }
+            }`,
+            { id: (resource as any).id },
+            jwt
+          );
+
+          expect(result.errors).not.toBeDefined();
+          expect(
+            (result.data as ExecutionResultDataDefault)[queryName]
+          ).toBeObject();
+        });
+      });
+    });
+  };
+}
---
+++ testing/backend/helpers.ts
@@ -0,0 +1,56 @@
+import mongoose from 'mongoose';
+import { ObjectId } from 'mongodb';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import { graphql, GraphQLSchema } from 'graphql';
+
+/**
+ * Helper function to generate ObjectID, note it returns the hex string of the ObjectId
+ */
+export const newId = () => {
+  return (mongoose.Types.ObjectId().toHexString() as unknown) as ObjectId;
+};
+
+export const runQuery = function(schema: GraphQLSchema) {
+  return async (
+    query: string,
+    variables: { [prop: string]: any },
+    token: string
+  ) => {
+    return graphql(
+      schema,
+      query,
+      null,
+      {
+        state: {},
+        token
+      },
+      variables
+    );
+  };
+};
+
+/**
+ * Helper function to setup Mongo Memory server
+ */
+export async function setupTestDB(): Promise<{
+  db: mongoose.Mongoose;
+  mongoServer: MongoMemoryServer;
+}> {
+  const mongoServer = new MongoMemoryServer();
+  const mongoUri: string = await mongoServer.getConnectionString();
+  const mongooseOpts: mongoose.ConnectionOptions = {
+    promiseLibrary: Promise,
+    autoReconnect: true,
+    reconnectTries: Number.MAX_VALUE,
+    reconnectInterval: 1000,
+    useNewUrlParser: true
+  };
+
+  const db: mongoose.Mongoose = await mongoose.connect(mongoUri, mongooseOpts);
+  console.log(`MongoDB successfully connected to ${mongoUri}`);
+
+  return {
+    db: db,
+    mongoServer: mongoServer
+  };
+}
