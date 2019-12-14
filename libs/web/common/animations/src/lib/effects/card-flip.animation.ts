db646b52b33b570886fd0cbbcc996566d34ae468
Jonathon Adams <jono.adams22@gmail.com>
2019-12-14 14:04:08 +1030

    fix(animations): remove internal function call of card flip

--- libs/web/common/animations/src/lib/effects/card-flip.animation.ts
+++ libs/web/common/animations/src/lib/effects/card-flip.animation.ts
@@ -3,8 +3,7 @@ import {
   style,
   query,
   animateChild,
-  animate,
-  keyframes
+  animate
 } from '@angular/animations';
 
 /**
@@ -14,70 +13,44 @@ import {
  * ease-in easing as the card entering use ease-out. This is because you want the
  * perception that at the midway point is at its fastest.
  */
-export function routerCardFlipAnimation({
-  flipLeft = false,
-  timingEnter = '0.2s',
-  timingExit = '0.2s'
-} = {}) {
-  let enterFlip = 'rotateY(90deg)',
-    exitFlip = 'rotateY(-90deg)';
 
-  if (flipLeft) {
-    enterFlip = 'rotateY(-90deg)';
-    exitFlip = 'rotateY(90deg)';
-  }
+export const routerCardFlipAnimation = animation([
+  // With routing changes, the new route component is inserted as the first child
+  // and the leaving component is the second child element
 
-  return animation(
-    [
-      // With routing changes, the new route component is inserted as the first child
-      // and the leaving component is the second child element
+  // First set, the parent to be relative positioning
+  style({ position: 'relative' }),
 
-      // First set, the parent to be relative positioning
-      style({ position: 'relative' }),
+  // because fo flex-box styling, to maintain the hight set the leaving to relative until it has animated away
+  query(':leave', [style({ position: 'relative' })]),
+  query(':enter', [
+    style({ position: 'absolute', left: '10000%', height: '0px' })
+  ]),
 
-      // because fo flex-box styling, to maintain the hight set the leaving to relative until it has animated away
-      query(':leave', [style({ position: 'relative' })]),
-      query(':enter', [
-        style({ position: 'absolute', left: '10000%', height: '0px' })
-      ]),
+  // Allow the children of the leaving element to animate
+  query(':leave', animateChild()),
+  query(':leave', [
+    style({ transform: 'none' }),
+    animate('{{ timing }} ease-in', style({ transform: '{{ exitFlip }}' })),
+    // Once it has flipped, set to absolute positioning and move off screen
+    style({ position: 'absolute', left: '10000%' })
+  ]),
 
-      // Allow the children of the leaving element to animate
-      query(':leave', animateChild()),
-      query(':leave', [
-        style({ transform: 'none' }),
-        animate(
-          '{{ timingExit }} ease-in',
-          style({ transform: '{{ exitFlip }}' })
-        ),
-        // Once it has flipped, set to absolute positioning and move off screen
-        style({ position: 'absolute', left: '10000%' })
-      ]),
+  // Change the height of the parent component and set to absolute to stop any overflow
+  style({ position: 'relative', height: '*' }),
 
-      // Change the height of the parent component and set to absolute to stop any overflow
-      style({ position: 'relative', height: '*' }),
+  // move the card entered, back to the center, relative positioning for flex-box height
+  // and start it 90 degrees flipped
+  query(':enter', [
+    style({
+      left: 0,
+      position: 'relative',
+      transform: '{{ enterFlip }}'
+    }),
 
-      // move the card entered, back to the center, relative positioning for flex-box height
-      // and start it 90 degrees flipped
-      query(':enter', [
-        style({
-          left: 0,
-          position: 'relative',
-          transform: '{{ enterFlip }}'
-        }),
-
-        // animate to normal positions
-        animate('{{ timingEnter }} ease-out', style({ transform: 'none' }))
-      ]),
-      // Allow children to animate
-      query(':enter', animateChild())
-    ],
-    {
-      params: {
-        timingEnter,
-        timingExit,
-        enterFlip,
-        exitFlip
-      }
-    }
-  );
-}
+    // animate to normal positions
+    animate('{{ timing }} ease-out', style({ transform: 'none' }))
+  ]),
+  // Allow children to animate
+  query(':enter', animateChild())
+]);
--- libs/web/common/ui/auth/src/lib/components/auth-router.animations.ts
+++ libs/web/common/ui/auth/src/lib/components/auth-router.animations.ts
@@ -1,10 +1,25 @@
 import { trigger, transition, useAnimation } from '@angular/animations';
 import { routerCardFlipAnimation } from '@uqt/common/animations';
 
-const flip = routerCardFlipAnimation();
-const flipLeft = routerCardFlipAnimation({ flipLeft: true });
-
 export const authRouterAnimations = trigger('authRouterAnimations', [
-  transition('LoginPage => RegisterPage', useAnimation(flip)),
-  transition('RegisterPage => LoginPage', useAnimation(flipLeft))
+  transition(
+    'LoginPage => RegisterPage',
+    useAnimation(routerCardFlipAnimation, {
+      params: {
+        timing: '0.2s',
+        enterFlip: 'rotateY(90deg)',
+        exitFlip: 'rotateY(-90deg)'
+      }
+    })
+  ),
+  transition(
+    'RegisterPage => LoginPage',
+    useAnimation(routerCardFlipAnimation, {
+      params: {
+        timing: '0.2s',
+        enterFlip: 'rotateY(-90deg)',
+        exitFlip: 'rotateY(90deg)'
+      }
+    })
+  )
 ]);
