e5faac35cab32e0aec8cad5db87d3790c89705d0
Jonathon Adams <jono.adams22@gmail.com>
2020-01-06 22:49:09 +1030

    feat(examples): add custom preload strategy for lazy modules

---
+++ libs/web/data-access/router/src/lib/selective-preloading-strategy.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { PreloadingStrategy, Route } from '@angular/router';
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SelectivePreloadingStrategyService implements PreloadingStrategy {
+  preload(route: Route, load: () => Observable<any>): Observable<any> {
+    // load if the route has data: { preload: true}
+    if (route.data && route.data['preload']) {
+      return load();
+    } else {
+      return of(null);
+    }
+  }
+}
