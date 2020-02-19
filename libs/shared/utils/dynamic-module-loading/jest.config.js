86afeb068a594d540cec29a6f019adbe44a4b9cc
Jonathon Adams <jono.adams22@gmail.com>
2020-02-19 11:22:30 +1030

    chore(deps): update nrwl packages and jest-preset-angular

--- libs/shared/utils/dynamic-module-loading/jest.config.js
+++ libs/shared/utils/dynamic-module-loading/jest.config.js
@@ -4,7 +4,8 @@ module.exports = {
   coverageDirectory:
     '../../../../coverage/libs/shared/utils/dynamic-module-loading',
   snapshotSerializers: [
-    'jest-preset-angular/AngularSnapshotSerializer.js',
-    'jest-preset-angular/HTMLCommentSerializer.js'
+    'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
+    'jest-preset-angular/build/AngularSnapshotSerializer.js',
+    'jest-preset-angular/build/HTMLCommentSerializer.js'
   ]
 };
