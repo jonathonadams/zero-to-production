072f0e9c0c1ce2f850083de80979242f41161d03
JA <24870903+jonathonadams@users.noreply.github.com>
2020-05-21 15:11:46 +0930

    fix(styles): fix contrast issues for dark mode (#65)
    * improvement: improve the styling of links
    
    * refactor: remove anchor component and style anchors
    
    * style: include additional fornt weight for bold text

--- libs/common/ui/buttons/src/lib/common-ui-buttons.module.ts
+++ libs/common/ui/buttons/src/lib/common-ui-buttons.module.ts
@@ -1,14 +1,9 @@
 import { NgModule } from '@angular/core';
-import { ZtpAnchorComponent } from './ztp-anchor/ztp-anchor.component';
 import { RaisedButtonComponent } from './raised-button/raised-button.component';
 import { IconButtonComponent } from './icon-button/icon-button.component';
 
 @NgModule({
-  declarations: [
-    ZtpAnchorComponent,
-    RaisedButtonComponent,
-    IconButtonComponent,
-  ],
-  exports: [ZtpAnchorComponent, RaisedButtonComponent, IconButtonComponent],
+  declarations: [RaisedButtonComponent, IconButtonComponent],
+  exports: [RaisedButtonComponent, IconButtonComponent],
 })
 export class CommonUiButtonsModule {}
