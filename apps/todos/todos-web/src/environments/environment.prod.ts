3a2d196469e5517b1ead21ffd8e8ba072d05c951
JA <24870903+jonathonadams@users.noreply.github.com>
2020-06-25 10:52:31 +0930

    fix: add todo app dark theme background color (#78)

--- apps/todos/todos-web/src/environments/environment.prod.ts
+++ apps/todos/todos-web/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 // ZTP_AFTER_CLONE -> change the url to point to your own API server
-const serverUrl = 'https://fns.zero-to-production.dev';
+const serverUrl = 'https://api.zero-to-production.dev';
 
 export const environment = {
   production: true,
