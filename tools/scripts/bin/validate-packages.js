0d7290ff6e8967ee55da0ddd5c243133e9d530de
JA <24870903+jonathonadams@users.noreply.github.com>
2020-07-08 10:50:09 +0930

    chore: remove glob dependency (#85)

--- tools/scripts/bin/validate-packages.js
+++ tools/scripts/bin/validate-packages.js
@@ -1,19 +1,16 @@
 #!/usr/bin/env node
 'use strict';
 Object.defineProperty(exports, '__esModule', { value: true });
-const tslib_1 = require('tslib');
-const glob_1 = tslib_1.__importDefault(require('glob'));
-const util_1 = require('util');
-const fs_1 = tslib_1.__importDefault(require('fs'));
 const child_process_1 = require('child_process');
-const asyncGlob = util_1.promisify(glob_1.default);
-const fsPromise = fs_1.default.promises;
+const path_1 = require('path');
+const fs_1 = require('fs');
+const { readdir, writeFile, readFile } = fs_1.promises;
 (async function validatePackageJson() {
   console.log('Validating package.json files');
   const workSpaceDir = process.cwd();
   const rootPackage = `${workSpaceDir}/package.json`;
   const { dependencies, devDependencies } = await readPackageJson(rootPackage);
-  const packagePaths = await asyncGlob(`${workSpaceDir}/apps/**/package.json`);
+  const packagePaths = await findNestedPackages(`${workSpaceDir}/apps`);
   const childPackages = await Promise.all(packagePaths.map(readPackageJson));
   let packagesToSave = [];
   childPackages.forEach((childPackage, i) => {
@@ -43,12 +40,15 @@ const fsPromise = fs_1.default.promises;
       });
     }
     if (isEdited) {
-      packagesToSave.push({ path: packagePaths[i], package: childPackage });
+      packagesToSave.push({
+        path: packagePaths[i],
+        package: childPackage,
+      });
     }
   });
   await Promise.all(
     packagesToSave.map((toSave) =>
-      fsPromise.writeFile(toSave.path, JSON.stringify(toSave.package, null, 2))
+      writeFile(toSave.path, JSON.stringify(toSave.package, null, 2))
     )
   );
   await Promise.all(
@@ -57,7 +57,7 @@ const fsPromise = fs_1.default.promises;
   console.log('Finished validating package.json files');
 })();
 async function readPackageJson(path) {
-  const jsonPackage = await fsPromise.readFile(path);
+  const jsonPackage = await readFile(path);
   return JSON.parse(jsonPackage.toString());
 }
 function addPackageToGitCommit(path) {
@@ -68,5 +68,37 @@ function addPackageToGitCommit(path) {
       }
       resolve('Success');
     });
+  });
+}
+// async function* getFiles(dir: string): any {
+//   const dirents = await fsPromise.readdir(dir, { withFileTypes: true });
+//   for (const dirent of dirents) {
+//     const res = resolve(dir, dirent.name);
+//     if (dirent.isDirectory()) {
+//       yield* getFiles(res);
+//     } else {
+//       yield res;
+//     }
+//   }
+// }
+async function getFiles(dir) {
+  const dirents = await readdir(dir, { withFileTypes: true });
+  const files = await Promise.all(
+    dirents.map((d) => {
+      const subPath = path_1.resolve(dir, d.name);
+      if (d.isDirectory()) {
+        return getFiles(subPath);
+      } else {
+        return Promise.resolve([subPath]);
+      }
+    })
+  );
+  return Array.prototype.concat(...files);
+}
+async function findNestedPackages(dir) {
+  const f = await getFiles(dir);
+  return f.filter((f) => {
+    const p = f.split('/');
+    return p[p.length - 1] === 'package.json';
   });
 }
--- tools/scripts/src/validate-packages.ts
+++ tools/scripts/src/validate-packages.ts
@@ -1,10 +1,8 @@
 #!/usr/bin/env node
-import glob from 'glob';
-import { promisify } from 'util';
-import fs from 'fs';
 import { exec } from 'child_process';
-const asyncGlob = promisify(glob);
-const fsPromise = fs.promises;
+import { resolve } from 'path';
+import { promises as fsPromises } from 'fs';
+const { readdir, writeFile, readFile } = fsPromises;
 
 (async function validatePackageJson() {
   console.log('Validating package.json files');
@@ -14,10 +12,13 @@ const fsPromise = fs.promises;
 
   const { dependencies, devDependencies } = await readPackageJson(rootPackage);
 
-  const packagePaths = await asyncGlob(`${workSpaceDir}/apps/**/package.json`);
+  const packagePaths = await findNestedPackages(`${workSpaceDir}/apps`);
 
   const childPackages = await Promise.all(packagePaths.map(readPackageJson));
-  let packagesToSave: { path: string; package: any }[] = [];
+  let packagesToSave: {
+    path: string;
+    package: any;
+  }[] = [];
 
   childPackages.forEach((childPackage, i) => {
     let isEdited = false;
@@ -47,13 +48,16 @@ const fsPromise = fs.promises;
     }
 
     if (isEdited) {
-      packagesToSave.push({ path: packagePaths[i], package: childPackage });
+      packagesToSave.push({
+        path: packagePaths[i],
+        package: childPackage,
+      });
     }
   });
 
   await Promise.all(
     packagesToSave.map((toSave) =>
-      fsPromise.writeFile(toSave.path, JSON.stringify(toSave.package, null, 2))
+      writeFile(toSave.path, JSON.stringify(toSave.package, null, 2))
     )
   );
 
@@ -64,7 +68,7 @@ const fsPromise = fs.promises;
 })();
 
 async function readPackageJson(path: string) {
-  const jsonPackage = await fsPromise.readFile(path);
+  const jsonPackage = await readFile(path);
   return JSON.parse(jsonPackage.toString()) as {
     dependencies: { [key: string]: string };
     devDependencies: { [key: string]: string };
@@ -79,5 +83,41 @@ function addPackageToGitCommit(path: string) {
       }
       resolve('Success');
     });
+  });
+}
+
+// async function* getFiles(dir: string): any {
+//   const dirents = await fsPromise.readdir(dir, { withFileTypes: true });
+//   for (const dirent of dirents) {
+//     const res = resolve(dir, dirent.name);
+//     if (dirent.isDirectory()) {
+//       yield* getFiles(res);
+//     } else {
+//       yield res;
+//     }
+//   }
+// }
+
+async function getFiles(dir: string): Promise<string[]> {
+  const dirents = await readdir(dir, { withFileTypes: true });
+  const files: string[][] = await Promise.all(
+    dirents.map((d) => {
+      const subPath = resolve(dir, d.name);
+      if (d.isDirectory()) {
+        return getFiles(subPath);
+      } else {
+        return Promise.resolve([subPath]);
+      }
+    })
+  );
+
+  return Array.prototype.concat(...files);
+}
+
+async function findNestedPackages(dir: string): Promise<string[]> {
+  const f = await getFiles(dir);
+  return f.filter((f) => {
+    const p = f.split('/');
+    return p[p.length - 1] === 'package.json';
   });
 }
