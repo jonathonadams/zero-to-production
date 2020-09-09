d425d65f85e3110b4bd390bfa1a5495d80b0a985
Jonathon Adams <24870903+jonathonadams@users.noreply.github.com>
2020-09-09 11:24:19 +0930

    fix(auth): add domain to refresh cookie for demo site

--- libs/server/auth/src/lib/core/cookies.ts
+++ libs/server/auth/src/lib/core/cookies.ts
@@ -6,6 +6,10 @@ const options: SetOption = {
   secure: true,
   sameSite: 'strict',
   path: '/authorize', // set to only the /authorize path so it is sent on both /refresh & /revoke
+  // ZTP_AFTER_CLONE You may want to delete the domain below so that it defaults to the host
+  // This is set so the the demo site works and the cookie is sent to both
+  // https://api.zero-to-production.dev && https://fns.zero-to-production.dev
+  domain: 'zero-to-production.dev',
 };
 
 export function setRefreshTokenCookie(cookies: Cookies, refreshToken?: string) {
